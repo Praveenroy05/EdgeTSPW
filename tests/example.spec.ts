bf1a126ea710e3cb0edd020a95ce746d84fa2ff0
Praveen <praveen.k@xnara.com>
2025-09-15 09:35:26 +0530

    added async-await

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+   await page.goto('https://playwright.dev/');
+   await page.getByRole('link', { name: 'Get started' }).click();
+   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test("Login test with valid credential", async function(){
+  // await Launch the url
+  // await Fill the username
+  // await Fill the password
+  // await Click on Login button
+  // await expect - Verify the login is successful
+})
+
+
+// async- await 
+
+// JS, TS - Asynchronous  - Parallel - 
+
+// Java - Synchronous - sequential - step by step
+
+// Promise - pending, successful , rejected
+
+// To run all the test case inside project - npx playwright test
+// To run a single file - npx playwright test tests/filename.spec.ts
+// To run a single test case - mark the test as test.only()
+// To run a single test case inside the project - npx playwright test --grep "title of the test case"
