9d5d4e0e8c9e378a5f0e560a68b2a9da24264ebe
Praveen <praveen.k@xnara.com>
2025-10-10 09:07:01 +0530

    added mouse operation

--- tests/mouseOperations.spec.ts
+++ tests/mouseOperations.spec.ts
@@ -22,3 +22,37 @@ test("Right click and Double click", async ({page})=>{
 
     // dialog event - It is used to handle the alert, confirm and prompt popups
 })
+
+test("Mouse Over on an element", async ({page})=>{
+    await page.goto("https://www.spicejet.com/")
+    // hover() - It will perform mouse over on an element
+    await page.getByText("Add-ons", {exact: true}).hover()
+    await page.getByTestId("test-id-Travel Assistance Services").click()
+    await page.waitForTimeout(3000)
+})
+
+test("Scrolling inside the page", async ({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    await page.getByText("Download Files", {exact: true}).scrollIntoViewIfNeeded()
+    await page.getByText("Download Files", {exact: true}).click()
+    await expect(page.locator("#generatePdf")).toBeVisible()
+})
+
+test("Drag and Drop operations", async ({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    const sourceElement= await page.locator("#draggable")
+    const targetElement = await page.locator("#droppable")
+
+    // dragTo() - It will perform drag and drop operation
+   // await sourceElement.dragTo(targetElement)
+    // await expect(targetElement).toHaveText("Dropped!")
+
+    // Alternative way of drag and drop using mouse events
+    await sourceElement.hover()
+    await page.mouse.down()
+    await targetElement.hover()
+    await page.mouse.up()
+    await expect(targetElement).toHaveText("Dropped!")
+
+
+})
