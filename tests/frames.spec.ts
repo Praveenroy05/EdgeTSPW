c38f2bb69006fd55064e28a6fa0e3616b337a5f0
Praveen Roy <pkroy0505@gmail.com>
2025-10-16 18:41:59 +0530

    added frames

---
+++ tests/frames.spec.ts
@@ -0,0 +1,38 @@
+// Frames - A web application integrated inside another web application
+import { test, expect } from '@playwright/test';
+
+test("Frames handling", async ({page})=>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    const inputElement = page.getByPlaceholder("Hide/Show Example")
+    await expect(inputElement).toBeVisible()
+    await page.locator("#hide-textbox").click()
+    await expect(inputElement).not.toBeVisible()
+    await expect(inputElement).toBeHidden()
+
+    // frameLocator() - to identify the frame
+    const framePage =  await page.frameLocator("#courses-iframe")
+    await framePage.getByText("All Access plan").first().click()
+    await expect(framePage.locator("div.inner-box")).toHaveText("All Access Subscription")
+    await expect(framePage.locator("div.inner-box")).toContainText("Subscription")
+
+    // Switch back to main page
+    await page.getByRole('button', {name:'Home'}).click()
+    await page.waitForTimeout(3000)
+})
+
+test("Nested frames handling", async ({page})=>{
+    await page.goto("https://demo.automationtesting.in/Frames.html")
+    await page.getByText("Iframe with in an Iframe").click()
+
+    const outerFrame = await page.frameLocator("#Multiple iframe")
+    const innerFrame = await outerFrame.frameLocator(".iframe-container iframe")
+    await innerFrame.locator("[type='text']").fill("Testing")
+    await expect(innerFrame.locator("[type='text']")).toHaveValue("Testing")
+
+    await page.getByText("Single Iframe ").click()
+
+})
+
+// Note: Avoid using frame() method as it is deprecated
+
+// File upload and shadow dow
