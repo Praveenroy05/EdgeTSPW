049426f3851b92710044e5397342cb32f83ab1e4
Praveen Roy <pkroy0505@gmail.com>
2025-10-22 09:04:42 +0530

    added codegen

--- tests/shadowdom.spec.ts
+++ tests/shadowdom.spec.ts
@@ -1,1 +1,11 @@
 // Shadow dom element
+
+import { test, expect } from "@playwright/test";
+test("Shadow dom element handling", async ({page})=>{
+    await page.goto("https://books-pwakit.appspot.com/explore?q=")
+    await page.locator("#input").fill("Testing")
+    await page.locator("div.icon").click()
+    await expect(page.locator("h2.title").filter({hasText:'Expert MySQL'}))
+    .toBeVisible()
+
+})
